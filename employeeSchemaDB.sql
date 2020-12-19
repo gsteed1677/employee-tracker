454666c76f829a5569e96dbee751e9583120f907
Garrett Steed <steed.garrett@gmail.com>
2020-12-18 18:18:00 -0700

    updated prompt fucntion and created seed.sql.  Code is working and prompts are functioning

--- employeeSchemaDB.sql
+++ employeeSchemaDB.sql
@@ -1,22 +1,24 @@
 DROP DATABASE IF EXISTs employee_trackerDB;
 CREATE DATABASE IF NOT EXISTS employee_trackerDB;
 
+USE employee_trackerDB;
+
 CREATE TABLE department(
-    id INT PRIMARY KEY,
-    name VARCHAR(30)
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role(
-    id INT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL(10, 2),
-    department_id INT
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INT NOT NULL
 );
 
 CREATE TABLE employee(
-    id INT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT NULL
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT
 )
--- employeeTracker.js
+++ employeeTracker.js
@@ -1,6 +1,11 @@
 const mysql = require('mysql');
 const inquirer = require("inquirer");
 
+//logo
+const logo = require('asciiart-logo');
+const config = require('./package.json');
+console.log(logo(config).render());
+
 
 //sql connection information
 var connection = mysql.createConnection({
@@ -8,19 +13,135 @@ var connection = mysql.createConnection({
     port: 3306,
     user: "root",
     password: "Regulater187$",
-    database: "need to make"
+    database: "employee_trackerDB"
 });
 
 //connection
 connection.connect((err) => {
   if (err) throw err;
+  console.log(`connected as id ${connection.threadId}\n`);
   runSearch();
 });
 
+//iquirer prompt for selection
 const runSearch = () => {
     inquirer
         .prompt({
+          message: "Choose which section you want to go through:",
+        type: "list",
+        choices: [
+            "view all employees",
+            "view all departments",
+            "add employee",
+            "add department",
+            "add role",
+            "update employee role",
+            "QUIT"
+        ],
+        name: "choice"
+    }).then(answers => {
+        console.log(answers.choice);
+        switch (answers.choice) {
+            case "view all employees":
+                viewEmployees()
+                break;
+
+            case "view all departments":
+                viewDepartments()
+                break;
+
+            case "add employee":
+                addEmployee()
+                break;
+
+            case "add department":
+                addDepartment()
+                break;
+
+            case "add role":
+                addRole()
+                break;
+
+            case "update employee role":
+                updateEmployeeRole();
+                break;
+
+            default:
+                connection.end()
+                break;
+          }
 
         })
 
+}
+
+const viewEmployees = () => {
+    connection.query("SELECT * FROM employee", function (err, data) {
+        console.table(data);
+        runSearch();
+    })
+}
+
+const viewDepartments = () => {
+    connection.query("SELECT * FROM department", function (err, data) {
+        console.table(data);
+        runSearch();
+    })
+}
+
+const addEmployee = () => {
+    connection.query("SELECT * FROM roles", function (err, data) {
+      if (err) throw err;
+  
+  inquirer
+    .prompt({
+      name: "firstName",
+      type: 'input',
+      message: 'What is employees first name?',
+
+  }).then((answer) => {
+    console.log(answer)
+    //filter method - filter.this
+
+  })
+
+})
+}
+
+const addDepartment = () => {
+  inquirer
+    .prompt({
+      name: "department",
+      type: 'input',
+      message: 'What is the name of the new department?',
+
+  }).then((answer) => {
+    console.log(answer)
+    let query = "INSERT INTO department (name) VALUES (?)";
+    connection.query(query, answer.department, function(err, res) {
+      console.log(`${(answer.department)} was added`)
+    })
+  viewDepartments();
+  })
+
+}
+
+const addRole = () => {
+    connection.query("SELECT * FROM roles", function (err, data) {
+      if (err) throw err;
+  
+  inquirer
+    .prompt({
+      name: "firstName",
+      type: 'input',
+      message: 'What is employees first name?',
+
+  })
+  .then((answer) => {
+    console.log(answer)
+    //filter method - filter.this
+
+  })
+
+})
 }
